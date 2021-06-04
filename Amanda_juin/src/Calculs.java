cb45ee74b92335bf3f249afe4192c7bce5e1da1c
amandatishler <amandatishler@amandas-mbp.lan>
2021-06-03 23:56:05 -0400

    3 juin

--- Amanda_juin/src/Calculs.java
+++ Amanda_juin/src/Calculs.java
@@ -19,31 +19,35 @@ public class Calculs extends Application {
 		Group group = new Group();
 		Scene scene = new Scene(group,400,400);
 		
-		TextField txt1 = new TextField();
-		TextField txt2 = new TextField();
-		Label lbl1 = new Label();
-		Label lblPlus = new Label();
-		Label lbl2 = new Label();
-		Label lblEgal = new Label();
-		Label lblRes = new Label();
+		TextField txt1 = new TextField("0");
+		TextField txt2 = new TextField("0");
+		Label lbl1 = new Label("0");
+		Label lblPlus = new Label("+");
+		Label lbl2 = new Label("0");
+		Label lblEgal = new Label("=");
+		Label lblRes = new Label("0");
 		
 		txt1.setLayoutX(120);
 		txt1.setLayoutY(100);
+		
 		txt2.setLayoutX(120);
 		txt2.setLayoutY(170);
+		
 		lbl1.setLayoutX(120);
 		lbl1.setLayoutY(250);
+		
 		lblPlus.setLayoutX(160);
 		lblPlus.setLayoutY(250);
+		
 		lbl2.setLayoutX(200);
 		lbl2.setLayoutY(250);
+		
 		lblEgal.setLayoutX(240);
 		lblEgal.setLayoutY(250);
+		
 		lblRes.setLayoutX(280);
 		lblRes.setLayoutY(250);
 		
-		lblPlus.setText("+");
-		lblEgal.setText("=");
 		
 		lbl1.textProperty().bind(txt1.textProperty());
 		lbl2.textProperty().bind(txt2.textProperty());
@@ -53,6 +57,7 @@ public class Calculs extends Application {
 		{
 			
 			
+			lblRes.setText(Integer.toString(checkText(lbl1) + checkText(lbl2)));
 
 		}
 				);
@@ -60,23 +65,28 @@ public class Calculs extends Application {
 		txt2.textProperty().addListener((observable,oldValue,newValue) -> 
 		{
 			
+			lblRes.setText(Integer.toString(checkText(lbl1) + checkText(lbl2)));
 			
 		}
 				);
 		
-		
-		
-		group.getChildren().add(txt1);
-		group.getChildren().add(txt2);
-		group.getChildren().add(lbl1);
-		group.getChildren().add(lblPlus);
-		group.getChildren().add(lbl2);
-		group.getChildren().add(lblEgal);
-		group.getChildren().add(lblRes);
+			
+		group.getChildren().addAll(txt1,txt2,lbl1,lblPlus,lbl2,lblEgal,lblRes);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 		
+	}
+	
+	int checkText (Label a)
+	{
+		int val;
+		
+		if(a.getText().equals(""))
+			val = 0;
+		else
+			val = Integer.parseInt(a.getText());
+		return val;
 	}
 
 	public static void main(String[] args) {
